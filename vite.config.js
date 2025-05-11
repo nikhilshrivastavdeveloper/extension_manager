a883822e70fd3dce194a80f3bafaa5d7b3785f5d
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-05-11 17:04:13 +0530

    add vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   server:{
-    host:"0.0.0.0",
-    fs:{
-      strict:false
-    }
-  },
+  //  server:{
+  //   host:"0.0.0.0",
+  //   fs:{
+  //     strict:false
+  //   }
+  // },
   plugins: [react(),tailwindcss()],
 })
