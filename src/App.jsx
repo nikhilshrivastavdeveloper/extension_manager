b2bd21f27c4fdf677f89c006397675a3de9e5d7a
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-05-11 20:34:18 +0530

    add dark mode with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 import Navbar from './components/nav.jsx'
 import Button from './components/Button.jsx'
@@ -11,7 +11,7 @@ function App() {
   const [activeExtension, setActiveExtensionData] = useState([]) // active data
   const [inActiveExtension, setInActiveExtension] = useState([]) // inactive data
   const [isDakModeOn, setDarkMode] = useState(true)
-    let body = useRef(null) // access html element
+  let body = useRef(null) // access html element
 
   //fetch data
   useEffect(() => {
@@ -24,7 +24,7 @@ function App() {
         console.log(err);
       }
     };
-  
+
     fetchData();
 
   }, [])
@@ -32,15 +32,15 @@ function App() {
   //for darkmode
   useEffect(() => {
     toggleDarkAndLightMode()
-  },[isDakModeOn])
+  }, [isDakModeOn])
 
   //toggle dark and light mode
   const toggleDarkAndLightMode = () => {
 
     console.log(body)
-    if(!body.current) return
+    if (!body.current) return
 
-    body.current.classList.remove("dark","light")
+    body.current.classList.remove("dark", "light")
 
     isDakModeOn ? body.current.classList.add("dark") : body.current.classList.add("light")
   }
@@ -53,19 +53,19 @@ function App() {
     setActiveExtensionData(newActiveExtensionList)
 
     let newInactiveExtensionList = inActiveExtension.filter((data) => (data.name !== name))
-    setInActiveExtension(newInactiveExtensionList) 
+    setInActiveExtension(newInactiveExtensionList)
   }
 
   const toggleActiveAndInactiveState = (name) => {
     let newExtensionList = extensionData.map((data) => (data.name === name ? { ...data, isActive: !data.isActive } : data))
     setExtensionData(newExtensionList)
 
-  // Refresh active and inactive lists
-  let activeList = newExtensionList.filter((data) => data.isActive);
-  let inactiveList = newExtensionList.filter((data) => !data.isActive);
+    // Refresh active and inactive lists
+    let activeList = newExtensionList.filter((data) => data.isActive);
+    let inactiveList = newExtensionList.filter((data) => !data.isActive);
 
-  setActiveExtensionData(activeList);
-  setInActiveExtension(inactiveList);
+    setActiveExtensionData(activeList);
+    setInActiveExtension(inactiveList);
   }
 
   const activeExtensionData = () => {
@@ -81,14 +81,14 @@ function App() {
   return (
     <div className='w-screen h-[100dvh] py-[20px] overflow-auto bg-gradient-to-t dark:from-[#091540] dark:to-[#040918] from-[#EBF2FC] to-[#EEF8F9]' ref={body}>
       <div className='w-[95%] sm:w-[82%] mx-auto'>
-        <Navbar darkModeFn={setDarkMode} isDakModeOn={isDakModeOn}  />
+        <Navbar darkModeFn={setDarkMode} isDakModeOn={isDakModeOn} />
 
         <div className=' mt-[20px] sm:mt-[40px] mb-[30px] flex flex-col items-center sm:flex-row sm:items-center sm:justify-between ' >
           <h1 className='dark:text-white text-[#121b40] text-[30px] font-bold'>Extensions List</h1>
           <div className='flex justify-around w-[100%] mt-[10px] gap-x-[12px] sm:mt-[0px] sm:w-auto'>
 
             <Button text="All" ID="all" trackState={activebtn} event={setActivebtn} />
-            <Button text="Active" ID="active" trackState={activebtn} event={setActivebtn} showList={activeExtensionData}/>
+            <Button text="Active" ID="active" trackState={activebtn} event={setActivebtn} showList={activeExtensionData} />
             <Button text="Inactive" ID="inactive" trackState={activebtn} event={setActivebtn} showList={inActiveExtensionData} />
 
           </div>
